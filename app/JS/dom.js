02804687d4f64caf2608464eb94d9cbf45a5a097
kaisbad7 <158204041+kaisbad@users.noreply.github.com>
2025-01-13 01:40:09 -0500

    asd

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,9 +1,5 @@
 const DOMSelectors = {
   container: document.querySelector(".container"),
-  playcontainer: document.querySelector(".playcontainer"),
-  button: document.querySelector(".btn"),
-  warm: document.getElementById("warm"),
-  cool: document.getElementById("cool"),
   rpsbtn: document.querySelector(".rpsbtn"),
   cfbtn: document.querySelector(".cfbtn"),
   rpscontainer: document.querySelector(".rpscontainer"),
--- app/JS/main.js
+++ app/JS/main.js
@@ -2,8 +2,6 @@ import "../CSS/style.css";
 import { DOMSelectors } from "../JS/dom.js";
 import { lcfb, coinFlip } from "../JS/options.js";
 
-console.log(lcfb, coinFlip);
-
 function playGameCard() {
   clearCards();
   lcfb.forEach((option) =>
@@ -27,10 +25,8 @@ function headTailsGameCard() {
     DOMSelectors.cfcontainer.insertAdjacentHTML(
       "beforeend",
       `<div class="playcard"><h1 class="header">${option.name}</h1>
-  <img src="${option.image}" alt="img" class="card-img">
-  <button class="cardbtn" data-id=${option.id}>Choose</button></div>
-  
-`
+      <img src="${option.image}" alt="img" class="card-img">
+      <button class="cardbtn" data-id=${option.id}>Choose</button></div>`
     )
   );
 }
@@ -39,33 +35,18 @@ function clearCards() {
   DOMSelectors.container.innerHTML = "";
 }
 
-DOMSelectors.rpsbtn.addEventListener("click", function (event) {
-  playGameCard();
-});
+function clearGameCards() {
+  DOMSelectors.rpscontainer.innerHTML = "";
+  DOMSelectors.cfcontainer.innerHTML = "";
+}
 
-DOMSelectors.cfbtn.addEventListener("click", function (event) {
-  headTailsGameCard();
-});
+function rpsGame(playerChoice) {
+  let random = Math.floor(Math.random() * 4);
+  let CPUChoice;
+  let outcome;
+  let playerNameChoice;
 
-DOMSelectors.cfcontainer.addEventListener("click", function (event) {
-  if (event.target.classList.contains("cardbtn")) {
-    let identifier = event.target.getAttribute("data-id");
-    console.log(identifier);
-  }
-});
-
-DOMSelectors.rpscontainer.addEventListener("click", function (event) {
-  if (event.target.classList.contains("cardbtn")) {
-    let identifier = event.target.getAttribute("data-id");
-    console.log(identifier);
-  }
-});
-
-function rpsGame() {
-  for (let round = 1; round <= 3; round++) {
-    let random = Math.floor(Math.random() * 4);
-    let CPUChoice;
-    //let outcome;
+  for (let i = 1; i <= 1; i++) {
     if (random === 0) {
       CPUChoice = "Bone";
     } else if (random === 1) {
@@ -75,11 +56,126 @@ function rpsGame() {
     } else {
       CPUChoice = "Fly";
     }
+    //0 = Lion
+    //1 = Chicken
+    //2 = Fly
+    //3 = Bone
+    if (CPUChoice == "Bone") {
+      if (playerChoice == 0) {
+        outcome = "lose";
+      } else if (playerChoice == 2) {
+        outcome = "win";
+      } else {
+        outcome = "tie";
+      }
+    } else if (CPUChoice == "Lion") {
+      if (playerChoice == 1) {
+        outcome = "lose";
+      } else if (playerChoice == 3) {
+        outcome = "win";
+      } else {
+        outcome = "tie";
+      }
+    } else if (CPUChoice == "Chicken") {
+      if (playerChoice == 2) {
+        outcome = "lose";
+      } else if (playerChoice == 0) {
+        outcome = "win";
+      } else {
+        outcome = "tie";
+      }
+    } else {
+      //fly
+      if (playerChoice == 3) {
+        outcome = "lose";
+      } else if (playerChoice == 1) {
+        outcome = "win";
+      } else {
+        outcome = "tie";
+      }
+    }
+    if (playerChoice == 0) {
+      playerNameChoice = "Lion";
+    } else if (playerChoice == 1) {
+      playerNameChoice = "Chicken";
+    } else if (playerChoice == 2) {
+      playerNameChoice = "Fly";
+    } else {
+      playerNameChoice = "Bone";
+    }
   }
-  console.log(CPUChoice);
+  clearGameCards();
+  DOMSelectors.rpscontainer.insertAdjacentHTML(
+    "beforeend",
+    `<div class="outcomecard"><h1 class="header">You ${outcome}!</h1>
+      <ul><li>AI Choice: ${CPUChoice}</li>
+      <li>Your Choice: ${playerNameChoice}</li></ul>
+      <h2>Refresh to Play Again</h2><div>`
+  );
 }
 
-rpsGame();
-//make font variables
-//make themes
-//separate home buttons
+function cfGame(playerChoice) {
+  let flip = Math.floor(Math.random() * 2);
+  let CPUChoice;
+  let outcome;
+  let playerNameChoice;
+
+  for (let i = 1; i <= 1; i++) {
+    if (flip === 0) {
+      flip = "Heads";
+    } else {
+      flip = "Tails";
+    }
+
+    if (playerChoice == 0) {
+      playerNameChoice = "Heads";
+    } else {
+      playerNameChoice = "Tails";
+    }
+
+    if (playerNameChoice == "Heads") {
+      CPUChoice = "Tails";
+    } else {
+      CPUChoice = "Heads";
+    }
+
+    if (playerNameChoice == flip) {
+      outcome = "win";
+    } else {
+      outcome = "lose";
+    }
+  }
+  clearGameCards();
+  DOMSelectors.cfcontainer.insertAdjacentHTML(
+    "beforeend",
+    `<div class="outcomecard"><h1 class="header">You ${outcome}!</h1>
+    <ul><li>Flip Outcome: ${flip}</li>
+    <li>AI Choice: ${CPUChoice}</li>
+    <li>Your Choice: ${playerNameChoice}</li></ul>
+    <h2>Refresh to Play Again</h2><div>`
+  );
+}
+
+DOMSelectors.rpsbtn.addEventListener("click", function () {
+  playGameCard();
+});
+
+DOMSelectors.cfbtn.addEventListener("click", function () {
+  headTailsGameCard();
+});
+
+//DOMSelectors.returnbtn.addEventListener("click", function () {});
+
+DOMSelectors.cfcontainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("cardbtn")) {
+    let playerChoice = event.target.getAttribute("data-id");
+    cfGame(playerChoice);
+  }
+});
+
+DOMSelectors.rpscontainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("cardbtn")) {
+    let playerChoice = event.target.getAttribute("data-id");
+    rpsGame(playerChoice);
+  }
+});
